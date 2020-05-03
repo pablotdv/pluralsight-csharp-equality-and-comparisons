e09df5021826a0192b24435ec920665c9d4f74c9
Pablo Tôndolo de Vargas <pablotdvsm@gmail.com>
2020-05-03 20:09:36 -0300

    C# Equality and Comparisons

---
+++ EqualIntegers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EqualIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int three = 3;
+            int threeAgain = 3;
+            bool intCmp = (three == threeAgain);
+            Console.WriteLine($"compare ints:           {intCmp}");
+
+            bool objCmp = ((object)three == (object)threeAgain);
+            Console.WriteLine($"compare objects:        {objCmp}");
+
+            bool itCmp = ((IComparable<int>)three == (IComparable<int>)threeAgain);
+            Console.WriteLine($"compare interfaces:     {itCmp}");
+        }
+    }
+}
